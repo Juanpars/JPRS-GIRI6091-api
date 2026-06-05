35389143defdd8132ef5a5619c7fe4422f2a3417
juanpars <jrps4949@gmail.com>
2026-06-04 20:35:00 -0600

    updates del dia 04/06/2026 estructura de tareas

--- src/tasks/domain/tasks.repository.interface.ts
+++ src/tasks/domain/tasks.repository.interface.ts
@@ -2,8 +2,10 @@ import { Task } from './tasks.entity';
 
 export interface ITasksRepository {
     create(task: Task): Promise<Task>;
-
     findById(id: string): Promise<Task | null>;
+    
+   
+    findAll(): Promise<Task[]>;
 }
 
 export const ITasksRepositoryToken = Symbol('ITasksRepository');
