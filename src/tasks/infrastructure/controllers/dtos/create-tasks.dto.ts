adaf952346469317dd7747c7834263820b1b7d39
juanpars <jrps4949@gmail.com>
2026-06-17 19:43:50 -0600

    updates del dia 11/06/2026 uso de prisma orm y configuracion de servicios y modulos

--- src/tasks/infrastructure/controllers/dtos/create-tasks.dto.ts
+++ src/tasks/infrastructure/controllers/dtos/create-tasks.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateTasksDto {
+  @ApiProperty({ 
+    description: 'Título de la tarea', 
+    example: 'Comprar leche', 
+    required: true 
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   @MaxLength(100)
   title!: string;
 
+  @ApiProperty({ 
+    description: 'Descripción detallada de la tarea', 
+    example: 'Ir al supermercado antes de que cierre', 
+    required: false 
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
